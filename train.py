3f6359d7de35e65458b16c68c60b9296b318ebac
An0n-xen <fyfybaisieamaoh6@gmail.com>
2023-07-08 01:29:00 +0100

    Update model code

--- train.py
+++ train.py
@@ -8,22 +8,22 @@ from torch.utils.data import Dataset, DataLoader
 
 from model import NeuralNet
 
-with open('traindata.json','r') as file:
+with open("traindata.json", "r") as file:
     intents = json.load(file)
-    
+
 all_words = []
 tags = []
 xy = []
 
-for intent in intents['intents']:
-    tag = intent['tag']
+for intent in intents["intents"]:
+    tag = intent["tag"]
     tags.append(tag)
-    for pattern in intent['patterns']:
+    for pattern in intent["patterns"]:
         w = tokenize(pattern)
         all_words.extend(w)
-        xy.append((w,tag))
-        
-ignore_words = ['?','!','.',',']
+        xy.append((w, tag))
+
+ignore_words = ["?", "!", ".", ","]
 all_words = [stem(w) for w in all_words if w not in ignore_words]
 all_words = sorted(set(all_words))
 tags = sorted(set(tags))
@@ -31,15 +31,18 @@ tags = sorted(set(tags))
 X_train = []
 y_train = []
 
-for (pattern_sentence,tag) in xy:
-    bag = bag_of_words(pattern_sentence,all_words)
-    X_train.append(bag)        
-    
+for pattern_sentence, tag in xy:
+    bag = bag_of_words(pattern_sentence, all_words)
+    X_train.append(bag)
+
     label = tags.index(tag)
     y_train.append(label)
-    
+
 X_train = np.array(X_train)
 y_train = np.array(y_train)
+
+# X + y
+# y + x
 
 
 class ChatDataset(Dataset):
@@ -47,58 +50,59 @@ class ChatDataset(Dataset):
         self.n_samples = len(X_train)
         self.x_data = X_train
         self.y_data = y_train
-        
-    def __getitem__(self,index):
+
+    def __getitem__(self, index):
         return self.x_data[index], self.y_data[index]
-    
+
     def __len__(self):
         return self.n_samples
-    
+
+
 batch_size = 8
-input_size = len(X_train[0])   
+input_size = len(X_train[0])
 hidden_size = 8
 output_size = len(tags)
-learning_rate = 0.001 
+learning_rate = 0.001
 num_epoch = 1000
-   
-dataset = ChatDataset()
-train_loader = DataLoader(dataset=dataset,batch_size=batch_size,num_workers=0,shuffle=True)
 
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-model = NeuralNet(input_size,hidden_size,output_size).to(device)
+dataset = ChatDataset()
+train_loader = DataLoader(
+    dataset=dataset, batch_size=batch_size, num_workers=0, shuffle=True
+)
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model = NeuralNet(input_size, hidden_size, output_size).to(device)
 
 criterion = nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(model.parameters(),lr=learning_rate)
- 
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
 for epoch in range(num_epoch):
-    for (words,labels) in train_loader:
+    for words, labels in train_loader:
         words = words.to(device)
         labels = labels.to(device)
-        
+
         outputs = model(words)
         loss = criterion(outputs, labels.long())
-        
-        
+
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
-       
-        
+
     if (epoch + 1) % 100 == 0:
-        print(f'epoch {epoch +1}/{num_epoch}, loss={loss.item():.4f}')
-        
-        
-print(f'final loss, loss={loss.item():.4f}')
+        print(f"epoch {epoch +1}/{num_epoch}, loss={loss.item():.4f}")
+
+
+print(f"final loss, loss={loss.item():.4f}")
 
 data = {
-    "model_state":model.state_dict(),
+    "model_state": model.state_dict(),
     "input_size": input_size,
     "output_size": output_size,
     "hidden_size": hidden_size,
-    "all_words":all_words,
-    "tags":tags
+    "all_words": all_words,
+    "tags": tags,
 }
 
 FILE = "data1.pth"
 torch.save(data, FILE)
-print(f'training complete, saved to:{FILE}')
+print(f"training complete, saved to:{FILE}")
